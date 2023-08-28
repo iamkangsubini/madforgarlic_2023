abe45a92c9f9c2736d87bc0c484909412d01e4ea
subin <pc666666@naver.com>
2023-08-28 16:06:06 +0900

    php도완료

---
+++ header.php
@@ -0,0 +1,13 @@
+<!-- header -->
+<header>
+        <!-- title -->
+        <section class="title">
+            <h1><a href="./index.html"><img src="./images/logo_white.png" alt="Mad for Garlic"></a></h1>
+            <nav>
+                <a href="./brand.html">BRAND</a>
+                <a href="./menu.html">MENU</a>
+                <a href="./store.html">STORE</a>
+                <a href="./reservation.html">RESERVATION</a>
+            </nav>
+        </section>
+    </header>
